d159b9ada711e76bfe7b50eb371d6c89fe627a3f
Marco <martinezramirezm82@gmail.com>
2026-09-20 03:24:16 -0600

    Se mueven de derecha a izquierda pero ahora si se mueven las piernas y los brazos de los personajes

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 5f;
 
     private Rigidbody2D rb;
+    private Animator animator;
     private float horizontal;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,16 +23,14 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Keyboard.current.aKey.isPressed ||
                 Keyboard.current.leftArrowKey.isPressed)
-            {
                 horizontal = -1f;
-            }
 
             if (Keyboard.current.dKey.isPressed ||
                 Keyboard.current.rightArrowKey.isPressed)
-            {
                 horizontal = 1f;
-            }
         }
+
+        animator.SetBool("IsMoving", horizontal != 0f);
     }
 
     private void FixedUpdate()
